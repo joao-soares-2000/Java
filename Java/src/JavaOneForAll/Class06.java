6357e800ea272fc20baf589393808f21d2371457
joao-soares-2000 <joao.159.soares@gmail.com>
2021-12-14 21:00:14 -0300

    New Package

---
+++ Java/src/JavaOneForAll/Class06.java
@@ -0,0 +1,25 @@
+package JavaOneForAll;
+
+public class Class06 {
+	public static void main(String[] args) {
+		/* age < 15 category junior
+		 age >= 15 && age < 18 category senior
+		 age >= 18 category masters*/
+		int age = 22;
+		String category;
+		
+		if (age<15 && age>0) {
+			category = "Junior";
+		}else if (age >=15 && age < 18) {
+			category = "Senior";
+		}else if (age >= 18) {
+			category = "Master";
+		}else {
+			category = "Invalid input";
+		}
+		System.out.println(category);
+		
+		
+	}
+
+}
