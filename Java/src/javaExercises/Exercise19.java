4109d9f8ceee74fd859b6bea35990f0023bb734a
joao-soares-2000 <joao.159.soares@gmail.com>
2021-12-23 22:06:34 -0300

    Switch case added

--- Java/src/javaExercises/Exercise19.java
+++ Java/src/javaExercises/Exercise19.java
@@ -4,45 +4,64 @@ import java.util.Scanner;
 
 public class Exercise19 {
 	public static void main(String[] args) {
+	Scanner scanner0 = new Scanner(System.in);		
+	System.out.println("Select what operation you want");
+	System.out.println("1 for Resistance - 2 for Tension - 3 for amperage - 4 for powerage");
+	int seletor = scanner0.nextInt();
+	switch (seletor) {
+		
 //A) R = U/I
-		/*Scanner scanner1 = new Scanner(System.in);
+	case 1:
+		Scanner scanner1 = new Scanner(System.in);
 		System.out.println("Volts ");
 		float volts = scanner1.nextFloat();
 		System.out.println("Ampers ");
 		float ampers = scanner1.nextFloat();
 		float resistence = volts/ampers;
 		scanner1.close();
-		System.out.println("The resistence will be " + resistence + " Ohms ");*/
+		scanner0.close();
+		System.out.println("The resistence will be " + resistence + " Ohms ");
+		break;
 		
 //B) U = R * I
-		/*Scanner scanner1 = new Scanner(System.in);
+	case 2:
+		Scanner scanner2 = new Scanner(System.in);
 		System.out.println("Ohms ");
-		float ohms = scanner1.nextFloat();
+		float ohms = scanner2.nextFloat();
 		System.out.println("Ampers ");
-		float ampers = scanner1.nextFloat();
+		ampers = scanner2.nextFloat();
 		float voltage = ohms/ampers;
-		scanner1.close();
-		System.out.println("The tension will be " + voltage + " volts ");*/
+		scanner2.close();
+		scanner0.close();
+		System.out.println("The tension will be " + voltage + " volts ");
+		break;
 		
 //C) I = U/R
-		/*Scanner scanner1 = new Scanner(System.in);
+	case 3: 
+		Scanner scanner3 = new Scanner(System.in);
 		System.out.println("Volts ");
-		float voltage = scanner1.nextFloat();
+		voltage = scanner3.nextFloat();
 		System.out.println("Ampers ");
-		float ohms = scanner1.nextFloat();
-		float ampers = voltage/ohms;
-		scanner1.close();
-		System.out.println("The amperage will be " + ampers + " ampers ");*/
+		ohms = scanner3.nextFloat();
+		ampers = voltage/ohms;
+		scanner3.close();
+		scanner0.close();
+		System.out.println("The amperage will be " + ampers + " ampers ");
+		break;
 		
 //D) P = U * I
-		Scanner scanner1 = new Scanner(System.in);
+	case 4:
+		Scanner scanner4 = new Scanner(System.in);
 		System.out.println("Volts ");
-		float voltage = scanner1.nextFloat();
+		voltage = scanner4.nextFloat();
 		System.out.println("Ampers ");
-		float ohms = scanner1.nextFloat();
+		ohms = scanner4.nextFloat();
 		float power = voltage*ohms;
-		scanner1.close();
-		System.out.println("The amperage will be " + power + " watts ");
+		scanner4.close();
+		scanner0.close();
+		System.out.println("The power will be " +  power + " watts");
+		break;
 		
+		}
 	}
 }
